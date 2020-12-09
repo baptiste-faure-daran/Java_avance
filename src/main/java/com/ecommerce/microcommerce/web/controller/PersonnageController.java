886eb4999a1cae98d5a00e87dccc812ff3b7f7b3
Baptiste Faure Daran <baptiste.faure-daran@le-campus-numerique.fr>
2020-12-09 20:51:20 +0100

    Commentaires Swagger en place

--- src/main/java/com/ecommerce/microcommerce/web/controller/PersonnageController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/PersonnageController.java
@@ -2,11 +2,13 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.PersonnageDao;
 import com.ecommerce.microcommerce.model.Personnage;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
+
 
 @RestController
 public class PersonnageController {
@@ -15,6 +17,7 @@ public class PersonnageController {
     private PersonnageDao IPersonnageDao;
 
     //Récupérer la liste des personnages
+    @ApiOperation(value = "Récupère tous les personnages existants")
     @RequestMapping(value = "/personnages", method = RequestMethod.GET)
     public List<Personnage> ListePersonnages() {
 
@@ -23,6 +26,7 @@ public class PersonnageController {
     }
 
     //Récupérer un personnage par son Id
+    @ApiOperation(value = "Récupère un personnage grâce à son ID à condition que celui-ci existe!")
     @GetMapping(value = "/personnages/{id}")
     public Personnage AfficherPersonnageParId(@PathVariable int id) {
 
@@ -30,14 +34,15 @@ public class PersonnageController {
 
     }
 
-    ;
-
+    //Permet d'ajouter un nouveau personnage
+    @ApiOperation(value = "Permet l'ajout d'un nouveau personnage à la liste")
     @PostMapping(value = "/personnages")
     public void AjouterPersonnage(@RequestBody Personnage personnage) {
         IPersonnageDao.save(personnage);
     }
 
     //Supprimer un personnage par son Id
+    @ApiOperation(value = "Supprime un personnage en se basant sur son ID")
     @RequestMapping(value = "/personnages/{id}/supprimer", method = RequestMethod.DELETE)
 
     public void SupprimerPersonnageParId(@PathVariable int id) {
