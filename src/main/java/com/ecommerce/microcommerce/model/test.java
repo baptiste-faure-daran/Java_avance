2d27d623a1ff3a37879269a1e217121fe49f265d
Baptiste Faure Daran <baptiste.faure-daran@le-campus-numerique.fr>
2021-02-10 14:05:13 +0100

    test

---
+++ src/main/java/com/ecommerce/microcommerce/model/test.java
@@ -0,0 +1,43 @@
+package com.ecommerce.microcommerce.model;
+
+public class Personnage {
+
+    public int id;
+    public String name;
+    public String classe;
+
+    public Personnage(int id, String name, String classe) {
+        this.id = id;
+        this.name = name;
+        this.classe = classe;
+
+    }
+
+    public String ToString() {
+        return "id :" + id + ", classe :" + classe + ", name :" + name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+}
